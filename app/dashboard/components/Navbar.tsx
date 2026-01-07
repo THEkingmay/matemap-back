63ab874967c31ca09b04ee08b517dd6aaf5f5e75
May3Dev <beerranee@gmail.com>
2026-01-07 21:15:22 +0700

    update ui mobile

--- app/dashboard/components/Navbar.tsx
+++ app/dashboard/components/Navbar.tsx
@@ -109,7 +109,7 @@ export default function DashboardNavbar() {
             <button 
               onClick={() => {
                 setIsMobileMenuOpen(false);
-                logout();
+               handleLogout()
               }} 
               className="w-full flex items-center gap-3 px-3 py-2 rounded-md text-base font-medium text-rose-600 hover:bg-rose-50 transition-colors text-left"
             >
