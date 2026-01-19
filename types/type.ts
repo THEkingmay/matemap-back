dc69c0cc642e21f4fe4dcbad43edf27b305a0191
May3Dev <beerranee@gmail.com>
2026-01-19 07:58:50 +0700

    add validation user

--- types/type.ts
+++ types/type.ts
@@ -5,12 +5,14 @@ interface User{
     role : 'admin' | 'user' | 'member' , // นิสิต แอดมิน และหอพักที่สมัครรายเดือน
     created_at : string
 }
-interface UserCard{
-  id : string , 
-  name : string ,
-  bio : string ,
-  avatar_url : string , 
-  tags : string[]
+interface UserCard {
+  id: string;
+  name: string;
+  age?: number;
+  faculty?: string;
+  major?: string;
+  tags?: string[];
+  image_url?: string;
 }
 
 export type {User , UserCard}
