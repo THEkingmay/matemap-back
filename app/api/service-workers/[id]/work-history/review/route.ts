bbe8bd4a93c2c59acdc5f941775a6c319997037e
WornWolf <thanakorn6661@gmail.com>
2026-02-01 02:22:10 +0700

    api review for pulling reviewer data

--- app/api/service-workers/[id]/work-history/review/route.ts
+++ app/api/service-workers/[id]/work-history/review/route.ts
@@ -1,30 +1,57 @@
 import { NextRequest, NextResponse } from "next/server";
 import supabase from "@/configs/supabase";
 
-export async function GET(req: NextRequest, ctx: { params: Promise<{ id: string }> }) {
-    try {
-        
-        const { id } = await ctx.params;
-        
-        if (!id) return NextResponse.json({ message: "No service worker ID" }, { status: 404 });
+export async function GET(
+  req: NextRequest,
+  ctx: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await ctx.params;
 
-        const { data, error } = await supabase
-            .from('service_review')
-            .select(`
-                id ,
-                review, 
-                rate,
-                service_history!inner (
-                    provider_id
-                )
-            `)
-            .eq('service_history.provider_id', id);
-
-        if (error) throw error;
-
-        return NextResponse.json(data, { status: 200 });
-
-    } catch (err) {
-        return NextResponse.json({ message: (err as Error).message }, { status: 500 });
+    if (!id) {
+      return NextResponse.json(
+        { message: "No service worker ID" },
+        { status: 400 }
+      );
     }
+
+    const { data, error } = await supabase
+    .from("service_review")
+    .select(`
+        id,
+        review,
+        rate,
+        created_at,
+        service_history!inner (
+        id,
+        start_date,
+        end_date,
+
+        services!service_history_service_type_id_fkey (
+            id,
+            name
+        ),
+
+        users!service_history_customer_id_fkey (
+            id,
+            user_detail (
+            name,
+            image_url
+            )
+        )
+        )
+    `)
+    .eq("service_history.provider_id", id)
+    .order("created_at", { ascending: false });
+
+
+    if (error) throw error;
+
+    return NextResponse.json(data, { status: 200 });
+  } catch (err) {
+    return NextResponse.json(
+      { message: (err as Error).message },
+      { status: 500 }
+    );
+  }
 }
