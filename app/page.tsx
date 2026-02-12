7b18c1301ef7b3bd74fd08230fcd28347c6c8a0d
Mymymay <151893237+THEkingmay@users.noreply.github.com>
2026-02-12 16:09:22 +0700

    Update download link for APK file

--- app/page.tsx
+++ app/page.tsx
@@ -51,7 +51,7 @@ export default function LandingPage() {
         {/* Action Buttons */}
         <div className="flex flex-col sm:flex-row gap-4 w-full justify-center items-center">
         
-        <a href="/เส้นทางสำหรับไฟล์.apk" download>
+        <a href="https://github.com/THEkingmay/matemap-front/releases/download/matemap/application-865a4319-ed65-46ca-849c-8f33cacdb45d.apk" download>
 
           <Button 
             size="lg" 
